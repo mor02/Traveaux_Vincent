e6d485c93e5ad6d83d9d85185588b3af879a5238
louferri <thelouferri@gmail.com>
2019-01-23 00:49:51 +0100

    avancement sur le TP

--- tp1_Vincent/src/com/tp/beans/Point.java
+++ tp1_Vincent/src/com/tp/beans/Point.java
@@ -1,10 +1,46 @@
 package com.tp.beans;
 
-public class Point {
+class Point {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int x,y;char l;
+	
+	private void check() {
+		if (x<0) x=0; if (y<0) y=0;
 	}
-
+	
+	void move (int dx, int dy) {
+		x+=dx; y+=dy; check();
+	}
+	
+	void set (int xi, int yi) {
+		x = xi; y = yi; check();
+	}
+	
+	int getX() {
+		return x;
+	}
+	
+	int getY() {
+		return y;
+	}
+	
+	void print() {
+		System.out.println(x+";"+y+":"+1);
+	}
+	
+	public static void main(String[] args) {
+			
+		Point pt = new Point();
+		pt.print();
+		pt.l = 'o';
+		pt.set(5,2);
+		pt.print();
+		pt.move(-6, 4);
+		pt.print();
+		
+		
+	}
+		
 }
+
+
